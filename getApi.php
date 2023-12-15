550598b2da49b5c259d13d59acccbe2bb61765d3
Desarrollador Full Stack | Experto en desarrollo web y aplicaciones | Programador | Velneo | Python <informaticolorenzo@gmail.com>
2023-12-15 23:37:46 +0100

    Update getApi.php

--- getApi.php
+++ getApi.php
@@ -1,4 +1,4 @@
-<?php //Este archivo  me lista los camiones
+<?php 
 	header('Access-Control-Allow-Origin: *');
 	$url="https://jsonplaceholder.typicode.com/users";
 	$json = file_get_contents($url); //Aqui tengo todo el contenido
