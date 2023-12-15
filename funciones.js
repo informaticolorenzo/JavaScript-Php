71b8a86d29c4bee14eb4c8bbd39d22b8426e67db
Desarrollador Full Stack | Experto en desarrollo web y aplicaciones | Programador | Velneo | Python <informaticolorenzo@gmail.com>
2023-12-15 23:31:35 +0100

    Update funciones.js

--- funciones.js
+++ funciones.js
@@ -1,20 +1,19 @@
+//Esta función se puede mejorar añadiendo control de errores y demás. Este solo es un ejemplo básico para empezar.
 function getApi() {
-	$('#tblUsuarios').DataTable({
-		 responsive: true,
-			"destroy": "true",
-				"ajax": {
-				"data": "",
-				"url": "php/getApi.php",
-				"type": "POST",
-				"dataSrc": "",
-			},
-			
-			"columns": [
-				{"data": "id"},
-				{"data": "name"},
-				{"data": "username"},
-				{"data": "email"},
-			],
-		});
-		
+    $('#tblUsuarios').DataTable({
+        responsive: true,
+        destroy: true,
+        ajax: {
+            data: "",
+            url: "php/getApi.php",
+            type: "POST",
+            dataSrc: "",
+        },
+        columns: [
+            { data: "id" },
+            { data: "name" },
+            { data: "username" },
+            { data: "email" },
+        ],
+    });
 }
